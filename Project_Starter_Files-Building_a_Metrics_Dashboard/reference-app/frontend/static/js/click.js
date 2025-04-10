1fa448551f2c62b12472d77f5b6998b1ce48a8c2
Uwe Sülter <uwe.suelter@jtelekom.de>
2025-04-10 13:40:44 +0200

    Deploying on namespace project3

--- Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/static/js/click.js
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/static/js/click.js
@@ -3,14 +3,14 @@ $(document).ready(function () {
     // all custom jQuery will go here
     $("#firstbutton").click(function () {
         $.ajax({
-            url: "http://backend.default.svc.cluster.local", success: function (result) {
+            url: "http://backend.project3.svc.cluster.local", success: function (result) {
                 $("#firstbutton").toggleClass("btn-primary:focus");
                 }
         });
     });
     $("#secondbutton").click(function () {
         $.ajax({
-            url: "http://trial.default.svc.cluster.local", success: function (result) {
+            url: "http://trial.project3.svc.cluster.local", success: function (result) {
                 $("#secondbutton").toggleClass("btn-primary:focus");
             }
         });
