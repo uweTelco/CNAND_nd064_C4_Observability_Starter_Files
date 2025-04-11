617cb424bc06ab82cc0fb659f9e9da38d090c063
Uwe Sülter <uwe.suelter@jtelekom.de>
2025-04-11 09:27:36 +0200

    Update to enable the frondend to call backend and trial microservice

--- Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/static/js/click.js
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/static/js/click.js
@@ -2,17 +2,37 @@ $(document).ready(function () {
 
     // all custom jQuery will go here
     $("#firstbutton").click(function () {
+        const button = $(this);
+        button.prop("disabled", true).html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...');
+        
         $.ajax({
-            url: "http://backend.project3.svc.cluster.local", success: function (result) {
-                $("#firstbutton").toggleClass("btn-primary:focus");
-                }
-        });
-    });
-    $("#secondbutton").click(function () {
-        $.ajax({
-            url: "http://trial.project3.svc.cluster.local", success: function (result) {
-                $("#secondbutton").toggleClass("btn-primary:focus");
+            url: "http://localhost:8081/api",
+            complete: function() {
+                button.prop("disabled", false).text("Press Me For A Good Test");
+            },
+            success: function (result) {
+                button.removeClass("btn-primary").addClass("btn-success");
+            },
+            error: function(xhr) {
+                button.removeClass("btn-primary").addClass("btn-danger");
             }
         });
+    });    
+    $("#secondbutton").click(function () {
+        const button = $(this);
+        button.prop("disabled", true).html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...');
+        
+        $.ajax({
+            url: "http://localhost:8082",
+            complete: function() {
+                button.prop("disabled", false).text("Press Me For A Good Test");
+            },
+            success: function (result) {
+                button.removeClass("btn-primary").addClass("btn-success");
+            },
+            error: function(xhr) {
+                button.removeClass("btn-primary").addClass("btn-danger");
+            }
+        });    
     });    
 });
