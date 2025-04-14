3f2b21d578a80c649268f3bacc305f38e4b661f0
Uwe Sülter <uwe.suelter@jtelekom.de>
2025-04-14 11:20:51 +0200

    Adding trace to backend app

--- Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/backend/app.py
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/backend/app.py
@@ -3,20 +3,45 @@ from flask import Flask, render_template, request, jsonify
 import pymongo
 from flask_pymongo import PyMongo
 from prometheus_flask_exporter import PrometheusMetrics
+from flask_opentracing import FlaskTracing
+from jaeger_client import Config
+from jaeger_client.metrics.prometheus import PrometheusMetricsFactory
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
 
 
 app = Flask(__name__)
-
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
 app.config["MONGO_DBNAME"] = "example-mongodb"
 app.config[
     "MONGO_URI"
 ] = "mongodb://example-mongodb-svc.default.svc.cluster.local:27017/example-mongodb"
+
 
 metrics = PrometheusMetrics(app)
 metrics.info("app_info", "Application info", version="1.0.3")
 
 mongo = PyMongo(app)
 
+def init_tracer(service):
+
+    config = Config(
+        config={
+            "sampler": {"type": "const", "param": 1},
+            "logging": True,
+            "reporter_batch_size": 1,
+        },
+        service_name=service,
+        validate=True,
+        metrics_factory=PrometheusMetricsFactory(service_name_label=service),
+    )
+
+    # this call also sets opentracing.tracer
+    return config.initialize_tracer()
+
+tracer = init_tracer("backend")
+flask_tracer = FlaskTracing(tracer, True, app)
 
 @app.route("/")
 def homepage():
@@ -25,8 +50,11 @@ def homepage():
 
 @app.route("/api")
 def my_api():
-    answer = "something"
-    return jsonify(repsonse=answer)
+    with tracer.start_span("call-api") as span:
+        answer = "something"
+        span.log_kv({"event": "trace it", "count": 3})
+        span.set_tag("api-call", answer)
+        return jsonify(repsonse=answer)
 
 
 @app.route("/star", methods=["POST"])
